d1b95d846d1c2856ed05777f8e79237b6f48ce87
pablov07 <pablovazquez606@gmail.com>
2024-01-22 13:10:56 -0800

    Added comments and kept Pylint score at 10

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # guessing_number
-Building a guessing number game with limited attempts in order to guess correctly. 
+Building a guessing number game with limited attempts in order to guess correctly. As well as getting a 10/10 score on Pylint.
+
--- guess_num.py
+++ guess_num.py
@@ -7,29 +7,35 @@ import sys
 
 print("Guessing Number Game! \n3 attempts to guess number correctly.")
 
+#Initializing the range of numbers to be guessed
 number = random.randint(1,9)
 
+#Initialze 0 chances have been taken
 CHANCES = 0
 
 print("Guess a number(1-9):")
 
 while True:
-
+    #User inputs guess
     guess = int(input())
 
+    #User guessed correctly!
     if guess == number:
-        print(f"YOU GUESSED CORRECTLY! THE NUMBER WAS {number} \
+        print(f"YOU GUESSED CORRECTLY! THE NUMBER WAS {number}\
               AND YOU DID IT IN {CHANCES} ATTEMPTS!")
         break
 
+    #Guess was low and asked to attempt again
     if guess < number:
         print(f"Guess was low. Guess again. (Attempt: {CHANCES+1})", guess)
+        CHANCES += 1
 
+    #Guess was high and asked to attempt again
     else:
         print(f"Guess was high. Guess again. (Attempt: {CHANCES+1})", guess)
+        CHANCES += 1
 
-    CHANCES += 1
-
+    #Limited attempts to 3 and quits program
     if CHANCES == 3:
         print("Ran out of chances. You Lose.")
         sys.exit(0)
